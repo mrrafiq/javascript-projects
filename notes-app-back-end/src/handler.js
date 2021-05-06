3cdd9f93d427c166fa96e82929a9d7e91d688a15
mrrafiq <rafiqmp3@gmail.com>
2021-05-06 16:49:40 +0700

    berhasil menghapus catatan

--- notes-app-back-end/src/handler.js
+++ notes-app-back-end/src/handler.js
@@ -102,4 +102,28 @@ const editNoteByIdHandler = (request, h) => {
     return response;
 };
 
-module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler };
+const deleteNoteByIdHandler = (request, h) => {
+    const {id} = request.params;
+
+    const index = notes.findIndex((note) => note.id === id);
+
+    if (index !== -1) {
+        notes.splice(index, 1);
+        const response = h.response({
+            status : 'success',
+            message : 'Catatan berhasil disimpan',
+        });
+        response.code(200);
+        return response;
+    }
+
+    const response = h.response({
+        status : 'fail',
+        message : 'Catatam gagal dihapus. Id tidak ditemukan',
+    });
+
+    response.code(404);
+    return response;
+};
+
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler };
