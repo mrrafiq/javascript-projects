18385e23c9ab019161f3d0acf3b794b011499be6
mrrafiq <rafiqmp3@gmail.com>
2021-05-06 15:38:29 +0700

    konfigurasi server.js

--- notes-app-back-end/src/server.js
+++ notes-app-back-end/src/server.js
@@ -1,1 +1,14 @@
-console.log("ASU");
+const Hapi = require('@hapi/hapi');
+
+const init = async() => {
+    const server = Hapi.server({
+        port : 5000,
+        host : 'localhost',
+    });
+
+    await server.start();
+
+    console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
